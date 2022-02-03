880f7fe3c8bafc024da2fb7f5f7862e48548e184
Juan <Juan_munozd@hotmail.es>
2022-02-03 08:42:25 -0500

    Parte1

---
+++ Parte 1/src/main/java/edu/escuelaing/ieti/service/UserService.java
@@ -0,0 +1,16 @@
+package edu.escuelaing.ieti.service;
+import edu.escuelaing.ieti.data.User;
+import java.util.List;
+public interface UserService {
+    
+    User create( User user );
+
+    User findById( String id );
+        
+    List<User> getAll();
+
+    void deleteById( String id );
+
+    User update( User user, String userId );
+    
+}
